7372c351f726b1397eefc4dc5b492ef77312bdd6
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-18 17:06:16 +0000

    fdsa

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,6 @@
+#include <stdio>
+int main(int arg)
+{
+puts('"Programming is like building a multilingual puzzle\n');
+return (0);
+}
---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: always 0 (Succes)
+ */
+int main(){
+  printf("some shit");
+  return 0;
+}
