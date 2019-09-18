ee7ce6a5658605e5b3cd0dc6fc672b86a8471d68
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-18 17:28:02 +0000

    das

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
-#include <stdio>
-int main(int arg)
+#include <stdio.h>
+#include <string.h>
+int main(char arg)
 {
-puts('"Programming is like building a multilingual puzzle\n');
+char string[80];
+strcpy(string, "\"Programming is like building a multilingual puzzle\n");
+puts(string);
 return (0);
 }
