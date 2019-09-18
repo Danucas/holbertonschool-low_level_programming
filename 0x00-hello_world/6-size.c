8f799cccf7ad1f5a86ab437a0de35a5ce2b3fcb7
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-18 19:25:22 +0000

    dsa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,10 @@
 */
 int main(void)
 {
-char c;
-int i;
-double d;
-long int li;
-long long int lli;
-float f;
-printf("Size of a char: %d byte(s)\n", sizeof(c));
-printf("Size of a int: %d byte(s)\n", sizeof(i));
-printf("Size of a long int: %d byte(s)\n", sizeof(li));
-printf("Size of a long long int: %d bytes(s)\n", sizeof(lli));
-printf("Size of a float: %d bytes(s)\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of a int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %d bytes(s)\n", sizeof(float));
 return (0);
 }
