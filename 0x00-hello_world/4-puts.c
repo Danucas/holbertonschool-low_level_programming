a61de8101c5d3c56db510a144d59b6f540ab896f
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-18 17:50:10 +0000

    das

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 /**
 *main - this is a description for main
 *Return: 0 cause its all right
 */
 int main(void)
 {
-char string = "\"Programming is like building a multilingual puzzle";
+char *string = ("\"Programming is like building a multilingual puzzle");
 puts(string);
 return (0);
 }
