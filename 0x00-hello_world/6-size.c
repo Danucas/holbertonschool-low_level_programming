c0bbda247f3531de914d756db9fc0e0ddbc68270
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-18 20:25:39 +0000

    dsa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int li = sizeof(long);
 int lli = sizeof(long long);
 int f = sizeof(float);
 printf("Size of a char: %d byte(s)\n", ch);
-printf("Size of a int: %d byte(s)\n", in);
+printf("Size of an int: %d byte(s)\n", in);
 printf("Size of a long int: %d byte(s)\n", li);
 printf("Size of a long long int: %d byte(s)\n", lli);
 printf("Size of a float: %d byte(s)\n", f);
