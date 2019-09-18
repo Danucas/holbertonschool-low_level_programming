8fec9610be334c4e55915366b798f9fb0f76c145
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-18 17:30:11 +0000

    dsa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main(char arg)
 {
 char string[80];
-strcpy(string, "\"Programming is like building a multilingual puzzle\n");
+strcpy(string, "\"Programming is like building a multilingual puzzle");
 puts(string);
 return (0);
 }
