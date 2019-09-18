d2282a0f578b35a81da43b736ecbe288b1c39d4d
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-18 17:46:53 +0000

    dsa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-int main(char arg)
+/**
+*main - this is a description for main
+*Return: 0 cause its all right
+*/
+int main(void)
 {
-char string[80];
-strcpy(string, "\"Programming is like building a multilingual puzzle");
+char string = "\"Programming is like building a multilingual puzzle";
 puts(string);
 return (0);
 }
