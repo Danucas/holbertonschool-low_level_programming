fb34a41a69c06e38c24da8f27a3d2cd751439a52
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-24 18:49:59 +0000

    das

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
-  *int_islower- it prints alphabet
+  *_isalpha- it prints alphabet
+  *@c: input var
   *Return: true if lower
  */
 int _isalpha(int c)
 {
-
-  if ((c >= 97 && c <= 123) ||(c >= 65 && c<=91))
+if ((c >= 97 && c <= 123) || (c >= 65 && c <= 91))
 {
 return (1);
 }
