a00a77ea59ec36cc639cc49f515bb6e721961be2
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-24 19:57:47 +0000

    fsd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,28 +2,27 @@
 #include <stdio.h>
 /**
 *print_to_98 - it prints alphabet
+*@a: input
 */
 void print_to_98(int a)
 {
 int pos;
 int add = 1;
-if(a > 98)
+if (a > 98)
 {
-  add = -1;
+add = -1;
 }
- for(pos = a; pos > 98 || pos < 98; pos += (add))
+for (pos = a; pos > 98 || pos < 98; pos += (add))
 {
 printf("%d", pos);
-if(pos != 98)
+if (pos != 98)
 {
 printf(", ");
 }
- else
-   {
-     break;
+else
+{
+break;
 }
 }
-
-
- printf("98\n");
+printf("98\n");
 }
