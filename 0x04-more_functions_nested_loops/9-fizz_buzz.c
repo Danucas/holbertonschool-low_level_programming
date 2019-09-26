814391c749fc0ecdacdbde4bc0fee50a3b68d0a3
danrocus1994 <danrodcastillo1994@gmail.com>
2019-09-26 18:23:53 +0000

    dsa

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,19 +9,23 @@ for (pos = 1; pos <= 100; pos++)
 {
 if ((pos % 3 == 0 && !(pos % 5 == 0)))
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if ((pos % 5 == 0 && !(pos % 3 == 0)))
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else if ((pos % 5 == 0 && pos % 3 == 0))
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else
 {
-printf("%d ", pos);
+printf("%d", pos);
+}
+if (pos != 100)
+{
+printf(" ");
 }
 }
 printf("\n");
