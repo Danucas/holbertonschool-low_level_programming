fc8177bb9416e5926c0553ddbc81bc88c21dbcc9
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-01 15:35:08 +0000

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,24 +7,28 @@ int getCharSize(char *s);
  */
 void puts2(char *str)
 {
-        
 	int pos;
 	int siz = getCharSize(str);
-      
-	for (pos = 0; pos < siz; pos+=2){
-		
+
+	for (pos = 0; pos < siz; pos += 2)
+	{
 		_putchar(str[pos]);
-	}	
+	}
 	_putchar('\n');
-    
 }
+/**
+ *getCharSize - leghtn
+ *@s:string to lenght
+ *Return:length
+ */
 
 int getCharSize(char *s)
 {
 	int co = 0;
-	while (s[co]!='\0')
-        {
-                co++;
-        }
+
+	while (s[co] != '\0')
+	{
+		co++;
+	}
 	return (co);
 }
