11dac8f7094946fb24ba34f48c23c9a0d3faf60f
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-01 15:40:08 +0000

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,24 +7,27 @@ int getCharSize(char *s);
  */
 void puts_half(char *str)
 {
-        
 	int pos;
 	int siz = getCharSize(str);
-      
-	for (pos = siz / 2; pos < siz; pos++){
-		
-		_putchar(str[pos]);
-	}	
-	_putchar('\n');
-    
-}
 
+	for (pos = siz / 2; pos < siz; pos++)
+	{
+		_putchar(str[pos]);
+	}
+	_putchar('\n');
+}
+/**
+ *getCharSize - length
+ *@s: legnth
+ *Return: length
+ */
 int getCharSize(char *s)
 {
 	int co = 0;
-	while (s[co]!='\0')
-        {
-                co++;
-        }
+
+	while (s[co] != '\0')
+	{
+		co++;
+	}
 	return (co);
 }
