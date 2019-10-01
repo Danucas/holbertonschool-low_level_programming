611de95dbdd966bdf354036eaebdf6a67d950fbe
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-01 15:53:27 +0000

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,33 +3,35 @@
 int getCharSize(char *s);
 /**
  *_strcpy - swap values
- *@a: char to size
- *@n: size
+ *@dest: char to size
+ *@src: size
+ *Return: pointer
  */
 char *_strcpy(char *dest, char *src)
 {
-       	int pos;
+	int pos;
 	int siz = getCharSize(src);
-      
-	for (pos = 0; pos < siz; pos++){
+
+	for (pos = 0; pos < siz; pos++)
+	{
 		dest[pos] = src[pos];
 	}
 	pos = 0;
-	while (pos < siz )
-	{
-		if (dest[pos] == '\0' && pos > 1)
-			printf("has null at end\nlast char: %c\n", dest[pos-1]);
-		pos++;
-	}
 	return (dest);
 }
+/**
+ *getCharSize - l
+ *@s: l
+ *Return:le
+ */
 
 int getCharSize(char *s)
 {
 	int co = 0;
-	while (s[co]!='\0')
-        {
-                co++;
-        }
+
+	while (s[co] != '\0')
+	{
+		co++;
+	}
 	return (co);
 }
