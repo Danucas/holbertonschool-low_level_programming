2f9b0bd5f1a58280b3590899702e0265a98497d1
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-01 15:06:36 +0000

    3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,16 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  *_puts - swap values
  *@str: char to size
  */
 void _puts(char *str)
 {
-        
 	int co = 0;
 
-	while (str[co]!='\0')
+	while (str[co] != '\0')
 	{
 		_putchar(str[co]);
 		co++;
 	}
-	
 	_putchar('\n');
-    
 }
