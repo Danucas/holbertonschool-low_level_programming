bf448dcb6198505d3861366ca74a3088fd40c198
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-03 23:52:58 +0000

    dsa

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdbool.h>
 /**
  *rot13 - concatnate
  *@s: dest pointer
@@ -8,23 +7,23 @@
 char *rot13(char *s)
 {
 	int pos = 0;
-	bool less = false;
-	bool greater = false;
+	int less = 0;
+	int greater = 0;
 
 	while (s[pos] != '\0')
 	{
 		less = s[pos] >= 65 && s[pos] < 78;
-		less = (s[pos] >= 97 && s[pos] < 110) || less;
+		less = (s[pos] >= 97 && s[pos] < 110) || less == 1;
 		greater = s[pos] >= 78 && s[pos] <= 90;
-		greater = (s[pos] >= 110 && s[pos] <= 122) || greater;
-		while (less || greater)
+		greater = (s[pos] >= 110 && s[pos] <= 122) || greater == 1;
+		while (less == 1 || greater == 1)
 		{
-			if (less)
+			if (less == 1)
 				s[pos] += 13;
 			else
 				s[pos] -= 13;
-			less = false;
-			greater = false;
+			less = 0;
+			greater = 0;
 			break;
 		}
 		pos++;
