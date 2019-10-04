190023364b489d1198e9d1b1ae5072f0a0ff82e2
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-04 00:31:13 +0000

    cdsa

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,24 +7,24 @@
 char *rot13(char *s)
 {
 	int pos = 0;
+	int i = 0;
 	int less = 0;
-	int greater = 0;
+	char comp[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	while (s[pos] != '\0')
 	{
-		less = s[pos] >= 65 && s[pos] < 78;
-		less = (s[pos] >= 97 && s[pos] < 110) || less == 1;
-		greater = s[pos] >= 78 && s[pos] <= 90;
-		greater = (s[pos] >= 110 && s[pos] <= 122) || greater == 1;
-		while (less == 1 || greater == 1)
+		i = 0;
+
+		while (i < 52 && (s[pos] != comp[i]))
 		{
-			if (less == 1)
-				s[pos] += 13;
-			else
-				s[pos] -= 13;
-			less = 0;
-			greater = 0;
-			break;
+			i++;
+		}
+		less = s[pos] == comp[i];
+		if (less == 1)
+		{
+			s[pos] = rot[i];
+			less  = 0;
 		}
 		pos++;
 	}
