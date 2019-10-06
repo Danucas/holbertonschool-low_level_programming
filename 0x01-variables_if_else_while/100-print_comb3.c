ecba36e18eb989b3b7871d17f927b3bc09bfdafe
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-05 22:44:51 -0500

    dsa

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,29 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 /**
  *main - prints depends on negative positive or zero
  *Return: 0 for great execution
  */
 int main(void)
 {
-int x;
-int y;
-for (x = 0; x < 10; x++)
-{
-  for (y = 0; y < 10; y++)
-    {
-      if (x < y)
-      {
-	putchar('0' + x);
-	putchar('0' + y);
-	if(x != 8)
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
 	{
-		putchar(',');
-		putchar(' ');
+		for (y = 0; y < 10; y++)
+		{
+			if (x < y)
+			{
+				putchar('0' + x);
+				putchar('0' + y);
+				if (x != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
-      }
-    }
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
