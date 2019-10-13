0388276636731b328f94f88a85b88e69737af4c1
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-13 00:48:41 -0500

    dsa

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	return (0);
 }
 /**
- *my_name -dsf
+ *mul -dsf
  *@argc: args count
  *@argv: 0 if todo esta good
 */
@@ -22,6 +22,7 @@ void mul(int argc, char *argv[])
 {
 	int mul1;
 	int mul2;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -29,45 +30,48 @@ void mul(int argc, char *argv[])
 	else
 	{
 		mul1 = _atoi(argv[1]);
-                mul2 = _atoi(argv[2]);
+		mul2 = _atoi(argv[2]);
 		printf("%d\n", mul1 * mul2);
 	}
 
 }
+/**
+ *_atoi -dsf
+ *@s: args count
+ *Return: dsad
+*/
 int _atoi(char *s)
 {
-        int pos = 0;
-        int sign = 1;
-        int res = 0;
-        int counting = 0;
+	int pos = 0;
+	int sign = 1;
+	int res = 0;
+	int counting = 0;
 
-        while (s[pos] != '\0')
-        {
-                if (s[pos] == '+')
-                {
-                        sign *= 1;
-                }
-                else if (s[pos] == '-')
-                {
-                        sign *= -1;
-                }
-                if (s[pos] >= 48 && s[pos] <= 57)
-                {
-                        counting = 1;
-                        if (res != 0)
-                        {
-                                /*printf("char: %c res: %d\t", s[pos], res);*/
-                                res = (res * 10) + (sign * (s[pos] - 48));
-                        }
-                        else
-                        {
-                                /*printf("char: %c res: %d\t", s[pos], res);*/
-                                res = sign * (s[pos] - 48);
-                        }
-                }
-                else if (counting == 1)
-                        break;
-                pos++;
-        }
-        return (res);
+	while (s[pos] != '\0')
+	{
+		if (s[pos] == '+')
+		{
+			sign *= 1;
+		}
+		else if (s[pos] == '-')
+		{
+			sign *= -1;
+		}
+		if (s[pos] >= 48 && s[pos] <= 57)
+		{
+			counting = 1;
+			if (res != 0)
+			{
+				res = (res * 10) + (sign * (s[pos] - 48));
+			}
+			else
+			{
+				res = sign * (s[pos] - 48);
+			}
+		}
+		else if (counting == 1)
+			break;
+		pos++;
+	}
+	return (res);
 }
