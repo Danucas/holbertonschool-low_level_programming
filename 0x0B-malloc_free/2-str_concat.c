0ae805045357c5e0594bd4e1dfdcb0695e693f02
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-15 21:54:30 +0000

    dsa

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,53 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-
-char  *str_concat(char *s1, char *s2)
+#include "holberton.h"
+/**
+ *str_concat - dsads
+ *@s1: dsa
+ *@s2: dsa
+ *Return: char pointer
+ */
+char *str_concat(char *s1, char *s2)
 {
 	char *buf;
 	unsigned int pos = 0;
 	unsigned int len = 0;
 	unsigned int f = 0;
 
-	while (pos != INT_MAX)
+	if (s1 == NULL)
+		f = 0;
+	else
 	{
-		if (f == 0)
+		while (*(s1 + len) != '\0')
 		{
-			if (*(s1 + pos) == '\0')
-			{
-				f = len;
-				pos = 0;
-
-			}
-			else
-				pos++;
+			len++;
+			f = len;
 		}
-		else if(f != 0)
-		{
-			if (*(s2 + pos) != '\0')
-				pos++;
-		        else
-			{
-				break;
-			}
-		}
-		len++;
 	}
-	//printf("%u", len);
+	if (s2 == NULL)
+		len = f;
+	else
+	{
+		pos = 0;
+		while (*(s2 + pos) != '\0')
+		{
+			len++;
+			pos++;
+		}
+	}
 	if (len == 0)
 		return (NULL);
-	buf = malloc(sizeof(char) * len);
+	buf = malloc(sizeof(char) * (len + 1));
 	pos = 0;
 	while (pos < len)
 	{
-//		printf("%d", pos);
 		if (pos < f)
 			buf[pos] = *(s1 + pos);
 		else
-			buf[pos] = *(s2 + (pos -f));
+			buf[pos] = *(s2 + (pos - f));
 		pos++;
 	}
-	buf[pos] = '\0';
-//	printf("%s", buf);
 	return (buf);
 }
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,15 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-char *_strcat(char *dest, char *src);
-int _putchar(char c);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
