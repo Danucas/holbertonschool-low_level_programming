a8f687499b350a23812c53cc7a5f2d1e3bc7a9a2
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-16 02:02:35 +0000

    dsa

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "holberton.h"
+/**
+ *_strlen - fdsafsa
+ *@s: dfs
+ *Return: dsa
+ */
+int _strlen(char *s)
+{
+	int co = 0;
+
+	while (s[co] != '\0')
+	{
+		co++;
+	}
+	return (co);
+}
+/**
+ *argstostr - dsads
+ *@ac: dsa
+ *@av: dsa
+ *Return: char pointer
+ */
+char *argstostr(int ac, char **av)
+{
+	char *buf;
+	int *lengths;
+	int final_len = 0;
+	int p;
+	int subp;
+	int count = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	lengths = malloc(sizeof(int) * (ac));
+	for (p = 0; p < ac; p++)
+	{
+		lengths[p] = _strlen(av[p]);
+		final_len += lengths[p] + 1;
+	}
+	buf = malloc(sizeof(char) * final_len);
+	if (lengths == NULL || buf == NULL)
+		return (NULL);
+	for (p = 0; p < ac; p++)
+	{
+		for (subp = 0; av[p][subp] != '\0'; subp++)
+		{
+			buf[count + subp] = av[p][subp];
+		}
+		buf[count + subp] = '\n';
+		count += lengths[p] + 1;
+	}
+	return (buf);
+}
