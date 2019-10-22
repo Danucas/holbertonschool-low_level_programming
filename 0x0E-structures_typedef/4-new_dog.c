6ff1ecf3c8448a700acf0cdf517764f8bafae40e
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-21 22:18:19 -0500

    dsa

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,56 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *new_dog - print a dog
+ *@name: th dog
+ *@age: som age
+ *@owner: some owner
+(* a blank line
+*Description: smiley is my dog my dog)?
+(* section header: header of this dog is dog.h)*
+ *Return: nothing
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *na, *ow;
+	int nl = 0, ol = 0;
+	dog_t newd;
+	dog_t *p;
+
+	while (*(name + nl) != '\0')
+	{
+		nl++;
+	}
+	nl++;
+	while (*(owner + ol) != '\0')
+	{
+		ol++;
+	}
+	ol++;
+	na = malloc(nl * sizeof(char));
+	if (na == NULL)
+		return (NULL);
+	nl = 0;
+	while (name[nl] != '\0')
+	{
+		na[nl] = name[nl];
+		nl++;
+	}
+	na[nl] = '\0';
+	ow = malloc(ol * sizeof(char));
+	if (ow == NULL)
+		return (NULL);
+	ol = 0;
+	while (owner[ol] != '\0')
+	{
+		ow[ol] = owner[ol];
+		ol++;
+	}
+	ow[ol] = '\0';
+	newd.name = na;
+	newd.age = age;
+	newd.owner = ow;
+	p = &newd;
+	return (p);
+}
