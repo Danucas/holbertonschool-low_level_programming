29557782dbed3c2b9bb69e3d412f8eebd7559584
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-21 20:38:00 -0500

    fds

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,8 +17,6 @@ void print_dog(struct dog *d)
 	if (d->name == 0)
 		printf("Name: (nil)\n");
 	printf("Name: %s\n", d->name);
-	if (age == 0)
-		printf("Age: (nil)\n");
 	printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
