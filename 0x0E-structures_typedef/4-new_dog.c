2fd77f0d30403326f874356beea833b6ca7e9878
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-21 22:37:03 -0500

    ·fds·

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,29 +15,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *na, *ow;
 	int nl = 0, ol = 0;
-	dog_t newd, *p;
+	dog_t *newd;
 
+	newd = malloc(sizeof(dog_t));
+	if (newd == NULL)
+		return (NULL);
 	while (*(name + nl) != '\0')
 		nl++;
-	nl++;
+	na = malloc((nl + 1) * sizeof(char));
+	if (na == NULL)
+	{
+		free(newd);
+		return (NULL);
+	}
 	while (*(owner + ol) != '\0')
 		ol++;
 	ol++;
-	na = malloc(nl * sizeof(char));
-	if (na == NULL)
+	ow = malloc(ol * sizeof(char));
+	if (ow == NULL)
+	{
+		free(newd);
+		free(na);
 		return (NULL);
+	}
 	for (nl = 0; name[nl] != '\0'; nl++)
 		na[nl] = name[nl];
 	na[nl] = '\0';
-	ow = malloc(ol * sizeof(char));
-	if (ow == NULL)
-		return (NULL);
 	for (ol = 0; owner[ol] != '\0'; ol++)
 		ow[ol] = owner[ol];
 	ow[ol] = '\0';
-	newd.name = na;
-	newd.age = age;
-	newd.owner = ow;
-	p = &newd;
-	return (p);
+	newd->name = na;
+	newd->age = age;
+	newd->owner = ow;
+	return (newd);
 }
