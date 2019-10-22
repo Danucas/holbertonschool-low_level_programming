1364f36116c0a01be6808ab4383870c9764f8052
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-21 22:43:15 -0500

    dsa

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *free_dog - print a dog
+ *@d: th dog
+(* a blank line
+*Description: smiley is my dog my dog)?
+(* section header: header of this dog is dog.h)*
+ *Return: nothing
+*/
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
