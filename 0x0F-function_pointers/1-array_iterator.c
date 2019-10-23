66d6955c9e7b2fdb340d859b980d9e0ba579640c
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-23 12:07:49 -0500

    keep pushing

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,8 +10,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && action != NULL && size > 0)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
