ee72a003e53e9c53cb62379b62d39c47275317f8
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-23 18:34:13 -0500

    keep pushing

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -24,6 +24,11 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	res = f(a, b);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
 /**
  * op_add - Struct op
  *
