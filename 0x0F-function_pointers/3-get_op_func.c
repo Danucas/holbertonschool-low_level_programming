5d42776f3c652e4ef98508879171118991c45573
danrocus1994 <danrodcastillo1994@gmail.com>
2019-10-23 18:26:41 -0500

    keep pushing

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,10 +21,9 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (ops[i].op != NULL && *(s) == ops[i].op[0])
-			break;
-
+		if (*(s) == ops[i].op[0])
+			return (ops[i].f);
 		i++;
 	}
-	return (*(ops[i]).f);
+	return (NULL);
 }
