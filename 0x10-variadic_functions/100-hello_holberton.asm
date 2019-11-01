c70ccefda4a6260ae1190828f316a741ab479c66
danrocus1994 <danrodcastillo1994@gmail.com>
2019-11-01 02:14:26 -0500

    das

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,17 @@
 	section .data                   ;
-	msg db "Hello, Holberton", 0xa	;
+	msg db "Hello, Holberton", 10	;
 	len equ $ - msg			;
 
 	section .text		;
 	global main		;
 
 main:				;
-	mov edx, len      	;
-	mov ecx, msg      ;
-	mov ebx, 1    ;
-	mov eax, 4      ;
+	mov rax, 1
+	mov rdi, 1      	;
+	mov rsi, msg      ;
+	mov rdx, len    ;
 	syscall	;
 
-	mov eax,1     ;
+	mov rax, 60     ;
+	mov rdi, 0
 	syscall      ;
