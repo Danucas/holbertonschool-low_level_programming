9e9650ad7d1e741fbba87223a77220cc6a8883b2
danrocus1994 <danrodcastillo1994@gmail.com>
2019-11-05 13:10:54 -0500

    free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,25 +3,23 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- *free_list - print list
+ *free_listint - print list
  *@head: the head of the list
  *Return: length
  */
-void free_list(list_t *head)
+void free_listint(listint_t *head)
 {
-	list_t *ne;
+	listint_t *ne;
 
 	if (head == NULL)
 		return;
 	while (head->next != NULL)
 	{
 		ne = head->next;
-		free(head->str);
 		free(head);
 		head = ne;
 	}
 	free(head->next);
-	free(head->str);
 	free(head);
 
 }
