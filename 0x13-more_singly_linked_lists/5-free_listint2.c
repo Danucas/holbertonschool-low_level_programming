e3e7061102e4778d4929333cde970c2ba655132b
danrocus1994 <danrodcastillo1994@gmail.com>
2019-11-05 13:20:34 -0500

    dsa

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *free_listint2 - print list
+ *@head: the head of the list
+ *Return: length
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ne = *head, *tmp;
+
+	if (ne == NULL)
+		return;
+	while (ne->next != NULL)
+	{
+		tmp = ne->next;
+		free(ne);
+		ne = tmp;
+	}
+	free(ne);
+	*head = NULL;
+}
