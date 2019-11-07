efac2b88d6e347189862d65554015f9928328d5d
danrocus1994 <danrodcastillo1994@gmail.com>
2019-11-07 15:48:55 -0500

    fds

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *get_bit - bin to dec
+ *@n: int to check
+ *@index: the index
+ *Return: void
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int grade = 1;
+	unsigned int pos = 0;
+	char str[65];
+
+	if (n == 0 && index == 0)
+	{
+		return (0);
+	}
+	grade = grade << 62;
+	while (!(n & grade))
+	{
+		grade >>= 1;
+	}
+	while (grade)
+	{
+		if (n & grade)
+		{
+			str[pos] = '1';
+		}
+		else
+		{
+			str[pos] = '0';
+		}
+		grade >>= 1;
+		pos++;
+	}
+	str[pos] = 0;
+	pos--;
+	if (index <= pos)
+		return (str[pos - index] - 48);
+	return (-1);
+}
