846740d7b2d211e27276f1d04191191d4c025dd2
danrocus1994 <danrodcastillo1994@gmail.com>
2019-11-07 15:57:59 -0500

    dsa

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,37 +10,10 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int grade = 1;
-	unsigned int pos = 0;
-	char str[65];
-
 	if (index > 63)
 		return (-1);
-	if (n == 0 && index == 0)
-	{
+	if (n & (1 << index))
+		return (1);
+	else
 		return (0);
-	}
-	grade = grade << 63;
-	while (!(n & grade))
-	{
-		grade >>= 1;
-	}
-	while (grade)
-	{
-		if (n & grade)
-		{
-			str[pos] = '1';
-		}
-		else
-		{
-			str[pos] = '0';
-		}
-		grade >>= 1;
-		pos++;
-	}
-	str[pos] = 0;
-	pos--;
-	if (index <= pos)
-		return (str[pos - index] - 48);
-	return (-1);
 }
