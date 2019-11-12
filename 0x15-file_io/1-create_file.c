3c5086782ca6bf8b3b8140eca2f431e7aaa02252
danrocus1994 <danrodcastillo1994@gmail.com>
2019-11-12 17:58:07 -0500

    sda

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,25 +13,30 @@
 
 int create_file(const char *filename, char *text_context)
 {
-	int fd;
+	int fd, ret = 1;
 	size_t s_len = 0;
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
-	fd = open(filename, O_RDWR | O_CREAT, 00600);
+	fd = open(filename, O_WRONLY | O_TRUNC);
 	if (fd < 0)
 	{
-		return (0);
+		close(fd);
+/*		printf("filedes: %d\n", fd);*/
+		fd = open(filename, O_WRONLY | O_CREAT, 00600);
+		if (fd < 0)
+			return (-1);
+/*		printf("filedes: %d %s\n", fd, text_context);*/
+		if (text_context != NULL)
+		{
+			while (*(text_context + (s_len)) != '\0')
+				s_len++;
+			write(fd, text_context, s_len);
+		}
 	}
-	while (*(text_context + (s_len)) != '\0')
-	{
-		s_len++;
-	}
-	write(fd, text_context, s_len);
-
 	close(fd);
 	(void) text_context;
-	return (1);
+	return (ret);
 }
