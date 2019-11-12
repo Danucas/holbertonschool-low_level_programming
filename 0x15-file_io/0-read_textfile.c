3e4b76c460879b775ec7846fb44b7530a667c0e9
danrocus1994 <danrodcastillo1994@gmail.com>
2019-11-12 13:08:40 -0500

    dsa

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -23,7 +23,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	if (fd < 3)
 	{
 		return (0);
 	}
