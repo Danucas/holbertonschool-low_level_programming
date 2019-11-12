840c9a5d3a110c7fe34aebcf9b5f76533155f538
danrocus1994 <danrodcastillo1994@gmail.com>
2019-11-12 16:51:32 -0500

    dsa

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ *create_file - read a file
+ *@filename: the file
+ *@text_context: amount of letters
+ *Return: letters printed
+ */
+
+int create_file(const char *filename, char *text_context)
+{
+	int fd;
+	size_t s_len = 0;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDWR | O_CREAT, 00600);
+	if (fd < 0)
+	{
+		return (0);
+	}
+	while (*(text_context + (s_len)) != '\0')
+	{
+		s_len++;
+	}
+	write(fd, text_context, s_len);
+
+	close(fd);
+	(void) text_context;
+	return (1);
+}
