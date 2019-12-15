02babcec2d0c20a96fa8cab3a3be1be10a6e6ee8
danrocus1994 <danrodcastillo1994@gmail.com>
2019-12-15 11:55:22 -0500

    dsa

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ *free_dlistint - free the elemnts of a double linked list
+ *@head: the header
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr = head;
+
+	while (ptr->next)
+	{
+		if (ptr->next)
+		{
+			ptr = ptr->next;
+			if (ptr->prev)
+				free(ptr->prev);
+		}
+		else
+		{
+			free(ptr);
+		}
+	}
+	if (ptr)
+		free(ptr);
+}
