7adf680f485e0e1896d48a56826579a53da75ad7
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-18 18:04:46 -0500

    dsa

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+/**
+ *add_hash_node - add a new node
+ *@top: top
+ *Return: void
+ */
+
+
+void add_hash_node(hash_node_t **top)
+{
+	hash_node_t *t = *top;
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (!t)
+	{
+		node->next = NULL;
+		*top = node;
+	}
+	else
+	{
+		while (t)
+		{
+			if (!t->next)
+			{
+				node->next = NULL;
+				t->next = node;
+				break;
+			}
+			t = t->next;
+		}
+	}
+}
+/**
+ *hash_table_create - add a new node
+ *@size: new has table size
+ *Return: hash table pointer
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int pos = 0;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_node_t *head = NULL;
+
+	add_hash_node(&head);
+	table->array = &head;
+	return (table);
+}
