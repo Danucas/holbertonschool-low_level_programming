24518657ea5b3a7f3f63bdce0bbf1b587170d625
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-18 18:44:35 -0500

    dsa

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,55 +1,20 @@
 #include "hash_tables.h"
 /**
- *add_hash_node - add a new node
- *@top: top
- *Return: void
- */
-
-
-hash_node_t *add_hash_node(hash_node_t **top)
-{
-	hash_node_t *t = *top;
-	hash_node_t *node = malloc(sizeof(hash_node_t));
-
-	if (!node)
-	{
-		return (NULL);
-	}
-	if (!t)
-	{
-		node->next = NULL;
-		*top = node;
-	}
-	else
-	{
-		while (t)
-		{
-			if (!t->next)
-			{
-				node->next = NULL;
-				t->next = node;
-				break;
-			}
-			t = t->next;
-		}
-	}
-	return (node);
-}
-/**
  *hash_table_create - add a new node
  *@size: new has table size
  *Return: hash table pointer
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int pos = 0;
 	hash_table_t *table = malloc(sizeof(hash_table_t));
 
+	if (size < 1)
+		return (NULL);
 	if (!table)
 		return (NULL);
-	table->array = malloc(sizeof(hash_node_t) * size);
+	table->array = (hash_node_t **) malloc(sizeof(hash_node_t) * size);
+	if (!table->array)
+		return (NULL);
 	table->size = size;
 	return (table);
-	(void) size;
-	(void) pos;
 }
