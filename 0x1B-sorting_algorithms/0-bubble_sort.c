b7cf14df6e536a1d8f967c261a626579eeb3756b
danrocus1994 <danrodcastillo1994@gmail.com>
2020-01-30 16:36:10 -0500

    Bubble sorted

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,10 +1,38 @@
 #include "sort.h"
 /**
+ *swap_bubble -  swapping function
+ *@array: the array
+ *@x: the position
+ */
+void swap_bubble(int *array, int x)
+{
+	int tmp = array[x];
+	array[x] = array[x + 1];
+	array[x + 1] = tmp;
+}
+
+/**
  *bubble_sort - sort a linked list using bubble algo
  *@array: the array to sort
  *@size: the array size
  */
 void bubble_sort(int *array, size_t size)
 {
-	
+	int x, y;
+	int flag = 0;
+
+	while (!flag)
+	{
+		flag = 1;
+		for (x = 0; x < size -1; x++)
+		{
+			if (array[x] > array[x + 1])
+			{
+				swap_bubble(array, x);
+				flag = 0;
+				print_array(array, size);
+			}
+		}
+	}
+
 }
