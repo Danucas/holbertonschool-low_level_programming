d017573bb1873f0697d85ff326ad7546a7038555
danrocus1994 <danrodcastillo1994@gmail.com>
2020-02-21 14:04:05 -0500

    dsa

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,14 +5,19 @@
 def island_perimeter(grid):
     """ Island perimeter function """
     count = 0
+    ing = False
     for y in range(1, len(grid)):
         for x in range(1, len(grid[y])):
             if (grid[y][x] == 1):
+                ing = True
                 ch_left = grid[y][x - 1]
                 ch_right = grid[y][x + 1]
                 ch_up = grid[y - 1][x]
                 ch_down = grid[y + 1][x]
                 count += 4 - (ch_left + ch_right + ch_up + ch_down)
+            else:
+                if ing is True:
+                    break
     return count
 
 
